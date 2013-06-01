2ec6f99352d534db2c5c0a09ea50cd2182d31d3b
Daniel Weigl <DanielWeigl@gmx.at>
2013-06-01 15:13:01 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenAB::Application.config.secret_token = '574c7a1fe361cee08ed0385e974b119b8b33efe96ee9a0c07089641c6239f660c01b19713f7564f176be31d16b32a6ae59b56fd3a03dfbf88a437d7927b7a9a3'
