718399dc695ada961a60b97f33e0e0f2533aa90f
Markus Dreier <mac@Mac-Book-Pro.local>
2013-06-01 19:35:26 +0200

    Login work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601140310) do
+ActiveRecord::Schema.define(:version => 20130601161650) do
 
   create_table "adresses", :force => true do |t|
     t.string   "vorname"
@@ -66,6 +66,15 @@ ActiveRecord::Schema.define(:version => 20130601140310) do
     t.string   "typ"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "usertyp"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
